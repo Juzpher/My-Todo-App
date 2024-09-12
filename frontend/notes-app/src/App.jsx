ae44717377bed8ce7ff8525c14e5b609fe7011b7
Juspher Vergara <87491192+EijiroNotME@users.noreply.github.com>
2024-09-12 15:13:33 +0800

    Update App.jsx

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,29 +1,28 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 
-const routes = (
-  <Router>
-    <Routes>
-      <Route path="/dashboard" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
-  </Router>
-);
-
 const App = () => {
+  console.log("App component rendered");
   return (
-    <>
-      {/* <div className="max-w-screen-2xl mx-auto">
-        <div className="min-h-[calc(100vh-96px)]">{routes}</div>
-      </div> */}
+    <Router>
       <div>
-        <div>{routes}</div>
+        <Routes>
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 };
+
 export default App;
